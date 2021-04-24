5acd96eb2feab3dd9731272012c035446a574de6
Hector Gonzalez <hectorgonzalezbarata@gmail.com>
2021-04-24 19:40:11 +0200

    Back to original

--- index.js
+++ index.js
@@ -17,17 +17,6 @@ function installSFDX(){
   exec(download+' && '+createDir+' && '+unzip+' && '+install+' && '+clean, function(error, stdout, stderr){
     if(error) throw(stderr)
     core.debug(stdout)
-    installGitDelta()
-    // if(core.getInput('sfdx-auth-url')) createAuthFile()
-  })
-}
-
-function installGitDelta() {
-  var install = 'echo y | sfdx plugins:install sfdx-git-delta'
-  var update = 'sfdx plugins'
-  exec(install+' && '+update, function(error, stdout, stderr){
-    if(error) throw(stderr)
-    core.debug(stdout)
     if(core.getInput('sfdx-auth-url')) createAuthFile()
   })
 }
